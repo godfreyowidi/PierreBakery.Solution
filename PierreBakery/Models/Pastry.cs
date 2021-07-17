332d19e2714c68c944841b25c07a722b0d1788c0
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-17 14:56:14 +0300

    add empty files

--- PierreBakery/Models/Pastry.cs
+++ PierreBakery/Models/Pastry.cs
@@ -5,24 +5,6 @@ namespace PierreBakery.Models
 {
   public class PastryQuantity
   {
-    public int Quantity { get; set; }
-    public PastryQuantity(int quantity)
-    {
-      Quantity = quantity;
-    }
-
-    public int CalcPastryCost(int quantity)
-    {
-      if(quantity % 3 == 0)
-      {
-        int totalCost = ((quantity / 3) * 5);
-        return totalCost;
-      }
-      else
-      {
-         return quantity * 2; 
-      }
-      
-    }
+    
   }
 }
