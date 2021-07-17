55ef0ac298274cae06bffef8964a903ee60fadca
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-17 15:21:35 +0300

    add good fail test for the cost of every two bread order

--- PierreBakery.Tests/ModelTests/PierreBakeryTests.cs
+++ PierreBakery.Tests/ModelTests/PierreBakeryTests.cs
@@ -14,5 +14,11 @@ namespace PierreBakery.Tests
       BreadQuantity testBreadCost = new BreadQuantity(1);
       Assert.AreEqual(5, testBreadCost.CalcBreadCost(1));
     }
+    [TestMethod]
+    public void CalcBreadCost_CostOfEveryTwoBreadOrder_10()
+    {
+    BreadQuantity testBreadCost = new BreadQuantity(3);
+    Assert.AreEqual(10, testBreadCost.CalcBreadCost(3));
+  }
   }
 }
