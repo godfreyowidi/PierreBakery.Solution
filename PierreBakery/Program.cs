cde60fecfa33635f0d99730cb0844d10197c4be4
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-19 11:09:55 +0300

    add error handling

--- PierreBakery/Program.cs
+++ PierreBakery/Program.cs
@@ -5,31 +5,38 @@ public class Program
 {
   static void Main()
   {
-    Console.WriteLine("           / |");
-    Console.WriteLine("          /  |");
-    Console.WriteLine("         /   |");
-    Console.WriteLine("        /    |");
-    Console.WriteLine("       /     |");
-    Console.WriteLine("      /      |");
-    Console.WriteLine("     /       |");
-    Console.WriteLine("    /        |");
-    Console.WriteLine("   / Welcome |");
-    Console.WriteLine("  /    To    |");
-    Console.WriteLine(" /           |");
-    Console.WriteLine("/PierreBakery|");
-    Console.WriteLine("-------------");
-    Console.WriteLine("Would you like to order bread or pastry? bread is $5 and pastry is $2 ");
-    Console.WriteLine("bread is $5 or you can buy 2 and get 1 free!");
-    Console.WriteLine("pastry is $2, or 3 for $5.");
-    Console.WriteLine("How many bread would you like to order?");
-    int bread = int.Parse(Console.ReadLine());
-    Console.WriteLine($"{bread} How many pastries would you like to order?");
-    int pastry = int.Parse(Console.ReadLine());
+    try
+    {
+      Console.WriteLine("           / |");
+      Console.WriteLine("          /  |");
+      Console.WriteLine("         /   |");
+      Console.WriteLine("        /    |");
+      Console.WriteLine("       /     |");
+      Console.WriteLine("      /      |");
+      Console.WriteLine("     /       |");
+      Console.WriteLine("    /        |");
+      Console.WriteLine("   / Welcome |");
+      Console.WriteLine("  /    To    |");
+      Console.WriteLine(" /           |");
+      Console.WriteLine("/PierreBakery|");
+      Console.WriteLine("-------------");
+      Console.WriteLine("Would you like to order bread or pastry? bread is $5 and pastry is $2 ");
+      Console.WriteLine("bread is $5 or you can buy 2 and get 1 free!");
+      Console.WriteLine("pastry is $2, or 3 for $5.");
+      Console.WriteLine("How many bread would you like to order?");
+      int bread = int.Parse(Console.ReadLine());
+      Console.WriteLine($"{bread} How many pastries would you like to order?");
+      int pastry = int.Parse(Console.ReadLine());
 
-    BreadQuantity Order1 = new BreadQuantity(bread);
-    PastryQuantity Order2 = new PastryQuantity(pastry);
+      BreadQuantity Order1 = new BreadQuantity(bread);
+      PastryQuantity Order2 = new PastryQuantity(pastry);
 
-    Console.WriteLine($"Your order is {bread} bread, {pastry} pastries");
-    Console.WriteLine($"Checkout: ${Order1.CalcBreadCost(bread) + Order2.CalcPastryCost(pastry)}");
+      Console.WriteLine($"Your order is {bread} bread, {pastry} pastries");
+      Console.WriteLine($"Checkout: ${Order1.CalcBreadCost(bread) + Order2.CalcPastryCost(pastry)}"); 
+    }
+    catch(Exception e)
+    {
+      Console.WriteLine(e.Message);
+    }
   }
 }
